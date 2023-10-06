02685a68e898d506d5cfcbaf78358d4809fa73ce
A3lequenne <clementlequenne1@gmail.com>
2023-10-06 16:45:14 +0200

    almost finished menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html",
+  content: ["./index.html", "./*.html",
             "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#ea580c",
+
+          "secondary": "#d6d3d1",
+
+          "accent": "#c2410c",
+
+          "neutral": "#111827",
+
+          "base-100": "#292524",
+
+          "info": "#51b0db",
+
+          "success": "#28d26c",
+
+          "warning": "#b91c1c",
+
+          "error": "#fa3842",
+        },
+      },
+    ],
+  },
+  plugins: [require('daisyui')],
 }
